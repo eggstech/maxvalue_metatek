4640398c12a528191f957a7cede83bb6d9a36dec
Cường Huỳnh <cuong.huynh@eggstech.io>
2025-09-25 03:34:25 +0000

    Thiếu background-color 🎨 Vấn đề: Nếu bạn đã sửa được vị trí nhưng vẫn t

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -131,8 +131,8 @@ export default function ReportsPage() {
             <Table className="min-w-full">
               <TableHeader className="sticky top-0 z-20 bg-muted">
                 <TableRow>
-                  <TableHead scope="col" rowSpan={2} className="sticky left-0 z-30 w-[120px] text-left bg-muted">Store Code</TableHead>
-                  <TableHead scope="col" rowSpan={2} className="sticky left-[120px] z-30 w-[180px] text-left bg-muted">Store Name</TableHead>
+                  <TableHead scope="col" rowSpan={2} className="sticky left-0 z-30 w-[120px] bg-muted text-left">Store Code</TableHead>
+                  <TableHead scope="col" rowSpan={2} className="sticky left-[120px] z-30 w-[180px] bg-muted text-left">Store Name</TableHead>
                   <TableHead scope="colgroup" colSpan={3} className="text-center border-l border-r">Job Overview</TableHead>
                   {DEPARTMENTS.map(dep => (
                     <TableHead key={dep} scope="colgroup" colSpan={3} className="text-center border-l border-r">{dep}</TableHead>

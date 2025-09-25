feaedd09430ce6f0016588c109e78fd38520da58
Cường Huỳnh <cuong.huynh@eggstech.io>
2025-09-25 03:36:55 +0000

    vẫn còn thấy khoảng trống giữa cột store code và store name (_for elemen

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -131,8 +131,8 @@ export default function ReportsPage() {
             <Table className="min-w-full">
               <TableHeader className="sticky top-0 z-20 bg-muted">
                 <TableRow>
-                  <TableHead scope="col" rowSpan={2} className="sticky left-0 z-30 w-[120px] text-left bg-muted">Store Code</TableHead>
-                  <TableHead scope="col" rowSpan={2} className="sticky left-[120px] z-30 w-[180px] text-left bg-muted">Store Name</TableHead>
+                  <TableHead scope="col" rowSpan={2} className="sticky left-0 z-30 w-[120px] bg-muted text-left">Store Code</TableHead>
+                  <TableHead scope="col" rowSpan={2} className="sticky left-[119px] z-30 w-[180px] bg-muted text-left">Store Name</TableHead>
                   <TableHead scope="colgroup" colSpan={3} className="text-center border-l border-r">Job Overview</TableHead>
                   {DEPARTMENTS.map(dep => (
                     <TableHead key={dep} scope="colgroup" colSpan={3} className="text-center border-l border-r">{dep}</TableHead>
@@ -160,7 +160,7 @@ export default function ReportsPage() {
                     return (
                         <TableRow key={row.storeCode} className="hover:bg-muted/30">
                             <TableCell className="font-medium sticky left-0 z-10 bg-card">{row.storeCode}</TableCell>
-                            <TableCell className="sticky left-[120px] z-10 bg-card">{row.storeName}</TableCell>
+                            <TableCell className="sticky left-[119px] z-10 bg-card">{row.storeName}</TableCell>
                             {/* Overview Data */}
                             <TableCell className="text-right border-l">{row.overview.total}</TableCell>
                             <TableCell className="text-right">{row.overview.completed}</TableCell>
